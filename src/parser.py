194f772ad54a8cce69cb84c3c1187e61fc3584a6
Karl N <nordenk@pm.me>
2021-05-07 09:23:06 -0700

    added comments

--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,10 @@
 From the Alpha Vantage API, we are going to use Intraday (Extended history)
 for our data set. Within this set (containing over 2 million minute-points),
 we will use the 60-minute-points instead to cut down on the size of our input.
+
+Because there is a limit to how many API calls a standard account can make per
+day, we were limited to a small data set of only a few companies in the list of
+S&P 500 companies.
 """
 import time
 from alpha_vantage.timeseries import TimeSeries
@@ -10,25 +14,6 @@ from alpha_vantage.sectorperformance import SectorPerformances
 from pprint import pprint
 from matplotlib import pyplot
 
-# MY_API_KEY = 'M4JP31006H3PKZ8T'
-
-
-# # getting the slice/snapshot of monthly data over 2 years
-# #-----------------------------------------------------------------------------
-# # each snapshot is 30-day.
-# slices = []
-# for y in range(1, 3):
-    # year = ""
-    # year += "year" + str(y)
-    # for i in range(1,13):
-        # month = ""
-        # month += year + "month" + str(i)
-        # slices.append(month)
-
-
-# building the dataset
-#-----------------------------------------------------------------------------
-DATA = dict()
 
 def writeData(path, data):
     """
@@ -46,7 +31,7 @@ def getData(equity, MY_API_KEY):
 
     :param equity (panda series): TimeSeries data and TechIndicators pandaframe.
     """
-    # ts = TimeSeries(key=f"{MY_API_KEY}", rapidapi=True, output_format='pandas', indexing_type='date')
+    # Pandaframe for TimeSeries
     ts = TimeSeries(key=f"{MY_API_KEY}", output_format='pandas', indexing_type='date')
     tsdata, tsmeta_data = ts.get_intraday(symbol=equity, interval='60min', outputsize='full')
     TS = tsdata.head(1000)
@@ -54,6 +39,7 @@ def getData(equity, MY_API_KEY):
     path += equity + ".csv"
     tsdata.to_csv(path_or_buf=path)
 
+    # Pandaframe for TechIndicators
     ti = TechIndicators(key=f"{MY_API_KEY}", output_format='pandas', indexing_type='data')
     tidata, timeta_data = ti.get_bbands(symbol=equity, interval='60min', time_period=60)
     TI = tidata.head(1000)
@@ -74,22 +60,28 @@ def getKeyList(keypath):
     return key_list
 
 
-def parseAllDataToCSV(company_list, key_list):
+def parseAllDataToCSV(company_list, key_list, DBUG=False):
     """
     make API calls and requests data for the given list of companies.
 
     :param company_list (list): list of companies to request data from
     :param key_list (list): list of keys to cycle through
+    :param DBUG (Bool): for debuggin purpose; only make API calls when False
     """
     j = 0  # starting from key at j position
     for i in range(len(company_list)):
-        if len(key_list) < 5:
+        # given a limited list of keys, we need to avoid exhausting the API call quota
+        if len(key_list) < 5:  
             time.sleep(15 / len(key_list))  # sleep for some time before making the next API call
 
-    # resets the key list if reached last key
+        # couple each symbol with a key, and make the API call
         print((company_list[i], key_list[j]))
-        getData(company_list[i], key_list[j])
-        if j == len(key_list) - 1:
-            j = 0
-        else:
-            j += 1
+        if not DBUG:
+            getData(company_list[i], key_list[j])
+
+        # resets the key list if reached last key
+        j = 0 if (j == len(key_list) - 1) else j + 1
+        # if j == len(key_list) - 1:
+            # j = 0
+        # else:
+            # j += 1
