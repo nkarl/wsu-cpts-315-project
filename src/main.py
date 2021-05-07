142b966909723c38f711752f1713e394290e3ef7
Karl N <nordenk@pm.me>
2021-05-07 06:22:23 -0700

    added a 1-minute wait between API calls

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 """This is the main module for our project.
 """
 import parser
+import time
 
 
 # test
 SP500_SYMBOLS = ['MMM', 'ABT', 'GOOGL', 'AMZN', 'MSFT']
 
 for symbol in SP500_SYMBOLS:
+    time.sleep(60)  # sleep for 1 minute before the next API call
     parser.getData(symbol)
