60de01ece61d3cab42478801cd4ee692543cecd8
Karl N <nordenk@pm.me>
2021-05-07 09:31:30 -0700

    added more comments

--- src/main.py
+++ src/main.py
@@ -1,22 +1,30 @@
-"""This is the main module for our project.
+"""
+This is the main module for our project.
 """
 import parser
-import time
 
-
+#-----------------------------------------------------------------------------
 # path to alpha vantage API keys
+#-----------------------------------------------------------------------------
 keypath = "apikey.key"
-mock_keypath = "mockkeys.key"
+# mock_keypath = "mockkeys.key"
 
+
+#-----------------------------------------------------------------------------
 # list of our companies to test
+#-----------------------------------------------------------------------------
 # SP500_SYMBOLS = ['MMM', 'ABT', 'GOOGL', 'AMZN', 'MSFT', 'FB', 'AAPL', 'JPM', 'TSLA', ]
 SP500_SYMBOLS = ['JNJ', 'NFLX', 'NVDA', 'UNH', 'WFC', 'C', 'COST', 'QCOM', 'PFE',
                  'INTC', 'BAC', 'PG', 'DIS']
 
+
+#-----------------------------------------------------------------------------
 # parse all data for the given list of companies
+#-----------------------------------------------------------------------------
 # parser.parseAllDataToCSV(SP500_SYMBOLS, parser.getKeyList(keypath), DBUG=True)
 
 
 """
 Next up, need to pick an algorithm
 """
+
